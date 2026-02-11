6f495660dfc0920aeeb73b620f4e2bf500d7f81d
Arnav Vats <vatsarnav17@gmail.com>
2026-02-11 22:39:41 +0530

    image 4 with silver jewelry

--- constants.tsx
+++ constants.tsx
@@ -5,7 +5,7 @@ export const MAIN_BG_IMAGES = [
   "assets/images/1.jpg",
   "assets/images/2.jpg",
   "assets/images/3.jpg",
-  "assets/images/4.jpg",
+  "assets/images/4.PNG",
   "assets/images/5.jpg",
   "assets/images/6.jpg",
 ];
