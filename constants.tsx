97045b2259b751bbe4408e3c61418bce9eec9ecd
Arnav Vats <vatsarnav17@gmail.com>
2026-02-11 17:48:54 +0530

    fix: change image paths to relative for gh-pages

--- constants.tsx
+++ constants.tsx
@@ -2,12 +2,12 @@
 import { Marker } from './types';
 
 export const MAIN_BG_IMAGES = [
-  "/assets/images/1.jpg",
-  "/assets/images/2.jpg",
-  "/assets/images/3.jpg",
-  "/assets/images/4.jpg",
-  "/assets/images/5.jpg",
-  "/assets/images/6.jpg",
+  "assets/images/1.jpg",
+  "assets/images/2.jpg",
+  "assets/images/3.jpg",
+  "assets/images/4.jpg",
+  "assets/images/5.jpg",
+  "assets/images/6.jpg",
 ];
 
 export const DEFAULT_BG_IMAGE = MAIN_BG_IMAGES[0];
